73fc71d97da17dacd5e46cfef31461fef2a93fc5
renanigt <renan_iguatu@hotmail.com>
2011-01-27 21:05:04 -0300

    Configuração inicial do projeto com VRaptor.

---
+++ src/br/com/mypaint/controller/IndexController.java
@@ -0,0 +1,25 @@
+package br.com.mypaint.controller;
+
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.Validator;
+
+@Resource
+public class IndexController {
+	
+	private final Result result;
+	
+	private Validator validator;
+	
+	public IndexController(Result result, Validator validator) {
+		this.result = result;
+		this.validator = validator;
+	}
+	
+	@Path("/")
+	public void index() {
+		result.include("vraptor", "VRaptor is working...");
+	}
+
+}
